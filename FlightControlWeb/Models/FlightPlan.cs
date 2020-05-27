061202acce4ff8399f2058417d5c8c40e0764c6a
roeezider <roeezider@gmail.com>
2020-05-27 11:43:17 +0300

    finishing server side

--- FlightControlWeb/Models/FlightPlan.cs
+++ FlightControlWeb/Models/FlightPlan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -8,16 +9,19 @@ namespace FlightControlWeb.Models
 {
     public class FlightPlan
     {
-
+        [JsonProperty("passengers")]
         [JsonPropertyName("passengers")]
         public int Passengers { get; set; }
+        [JsonProperty("company_name")]
         [JsonPropertyName("company_name")]
         public string Company_name { get; set; }
+        [JsonProperty("initial_location")]
         [JsonPropertyName("initial_location")]
         public InitialLocation InitialLocation { get; set; }
+        [JsonProperty("segments")]
         [JsonPropertyName("segments")]
         public List<Segment> Segments { get; set;}
-
+        /*
         public FlightPlan(int passengers, string companyName, InitialLocation location, List<Segment> segments)
         {
            
@@ -26,6 +30,7 @@ namespace FlightControlWeb.Models
             InitialLocation = location;
             Segments = segments;
         }
+        */
 
     }
 }
--- FlightControlWeb/Models/InitialLocation.cs
+++ FlightControlWeb/Models/InitialLocation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -8,10 +9,13 @@ namespace FlightControlWeb.Models
 {
     public class InitialLocation
     {
+        [JsonProperty("longitude")]
         [JsonPropertyName("longitude")]
        public  double Longitude{ get; set; }
+        [JsonProperty("latitude")]
         [JsonPropertyName("latitude")]
         public double Latitude{ get; set; }
+        [JsonProperty("date_time")]
         [JsonPropertyName("date_time")]
         public string Date_time { get; set; }
     }
