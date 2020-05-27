cce1192fbbc7f429e9ef1abc69eb3a248aae74b3
harel feldman <harel.feldman1@live.biu.ac.il>
2020-05-27 17:13:44 +0300

    Add html

---
+++ FlightControlWeb/wwwroot/javascript/UI.js
@@ -0,0 +1,25 @@
+﻿function updateFlights(flights) {
+    flights.forEach(function (flight, i) {
+        // Get flight id.
+        let idFlight = flight.flight_Id;
+        if (!flight.is_external) {
+            $('#flight_list').append('<tr onClick="showFlight(id)" id="' + idFlight + '"><td>' + idFlight + '</td><td> '
+                + flight.company_Name + '</td><td id="deleteButton"><button type="button" class="btn" value="Delete" id="' + "deleteRow" + count +
+                '"><span class="fa fa-close"></span><span class="submit - text"> Delete</span></button></td></tr>');
+        }
+        let x = document.getElementById("deleteRow" + count);
+        x.addEventListener("click", function () {
+            deleteRow(idFlight, x);
+        });
+    });
+}
+function showFlight(idFlight) {
+    // when click on row of flight - change color.
+
+
+    // get plane from map and change its color.
+
+
+
+    // TODO: show flight path
+}
